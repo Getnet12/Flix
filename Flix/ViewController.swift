dcc80d01c79da4041987644a684d38de0f8bd3d4
Getnet Mekuriyaw <getnetmekuriyaw@gmail.com>
2020-03-04 22:41:25 -0600

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Getnet Mekuriyaw on 3/4/20.
+//  Copyright © 2020 Getnet Mekuriyaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
