d23aac4407fe215c11675b7267c92540f3f8f307
Getnet Mekuriyaw <melkamugetnet1@gmail.com>
2021-12-05 23:18:19 -0600

    Added more details about movies

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Getnet Mekuriyaw on 12/5/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
